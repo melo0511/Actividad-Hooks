549a5bd9917be23dc6ced76f1f0f7ab6479cd604
Jhonatan Melo <josejhonatanxd@gmail.com>
2023-02-24 17:13:40 -0500

    Falta mandar los tweets al footer por props

--- src/Components/Layouts/Footer/Footer.jsx
+++ src/Components/Layouts/Footer/Footer.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Footer = () => {
   return (
     <footer id='footer'>
-        <h3>Aqui se veran sus tweets archivados</h3>
+
+        <div id='FooterTitle'>
+          <h3>Aqui se veran sus tweets archivados</h3>
+        </div>
+        
+        <div id='containerTweets'>
+          
+        </div>
+
     </footer>
   )
 }
--- src/Components/Layouts/Main/Main.jsx
+++ src/Components/Layouts/Main/Main.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
+import { useState } from 'react'
 import { Buttons } from '../../UI/Buttons/Buttons'
 
-export const Main = () => {
+const Tweets = []
+
+export const Main = (prop) => {
+
+    const [cap,capText] = useState("")
+
+    const captureText = (e)=>{
+
+        let value = e.target.value
+
+        if(value.length <= 255){
+            capText(value)
+            setCount(value.length)
+        }
+        
+    }
+
+    //Contador
+    const [count,setCount] = useState(0)
+
+    // Publicar
+    const [Add,setAdd] = useState('Aqui verás tu tweet actual')
+
+    const btnAdd = ()=>{
+        setAdd(cap)
+        capText("")
+    }
+
+    //Archivar
+
+    const btnArchive = ()=>{
+        let saveTweet = Add
+        Tweets.push(saveTweet)
+        console.log(Tweets);
+    }
+
+    //Mostrar archivados
+
+    const [archive,setArchive] = useState()
+
+    const btnShowArchive = ()=>{
+
+        if(Tweets.length===0){
+            setAdd("No has 'archivado' ningun tweet")
+        }else{
+            
+            let i= 0
+
+            Tweets.forEach(element => {
+
+                <p>{i+". "+element}</p>
+                i++
+                console.log(i+"."+element);
+
+            });
+
+        }
+
+    }
+
   return (
+
     <main id="main">
+
         <section id="principalContainer">
 
             <div id="head">
@@ -12,8 +74,8 @@ export const Main = () => {
 
             <div id='containerText'>
 
-                <textarea name="" id="textArea" placeholder="limite de caracteres '225'">
-
+                <textarea value={cap} onChange={captureText}  id="textArea" placeholder="limite de caracteres '225'">
+                    
                 </textarea>
 
             </div>
@@ -23,19 +85,19 @@ export const Main = () => {
                 <div id='containerButtons'>
 
                     <Buttons 
-                        click = ''
+                        click = {btnAdd}
                         identifier = 'Button'
                         textButton = 'Publicar'
                     />
 
                     <Buttons 
-                        click = ''
+                        click = {btnArchive}
                         identifier = 'Button'
                         textButton = 'Archivar'
                     />
 
                     <Buttons 
-                        click = ''
+                        click = {btnShowArchive}
                         identifier = 'Button'
                         textButton = 'Mostrar Archivados'
                     />
@@ -50,11 +112,11 @@ export const Main = () => {
                     </div>
 
                     <div id='text'>
-                        <p>Aqui se verá tu tweet</p>
+                        <p>{Add}</p>
                     </div>
 
                     <div id='containerCount'>
-                        <p>255</p>
+                        <p>{count}</p>
                     </div>
 
                 </div>
@@ -62,6 +124,7 @@ export const Main = () => {
             </div>
 
         </section>
+
     </main>
   )
 }
